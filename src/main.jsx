c2e3614589d2ba4f55d031abd8195c7b2b3944b9
Syedsubhan1129 <Syedsubhan031222@gmail.com>
2025-07-22 14:23:25 +0530

    made changes to router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx';
 import './index.css';
 
@@ -11,8 +10,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
